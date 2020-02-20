c791e33a1a3a6817e75b0eda33c658a470b15612
Nicholas Ashcraft <nick1721@live.unc.edu>
2020-02-19 19:28:35 -0500

    initial code to debug

--- TreasureHunter.cs
+++ TreasureHunter.cs
@@ -7,22 +7,39 @@ public class TreasureHunter : MonoBehaviour
 {
     
     public GameObject leftPointerObject;
+
     public GameObject rightPointerObject;
+
     public collectible[] collectiblesInScene;
+
     public TreasureHunterInventory inventory;
+
     public int score = 0;
+
     public TextMesh scoreSummary;
+
     public TextMesh itemSummary;
+
     public GameObject playerCamera;
-    public RaycastHit outHit;
+
+    //public RaycastHit outHit;
+
     public collectible collectedItem;
+
     public int numOfItems = 0;
 
+    public LayerMask collectiblesMask;
+
+    GameObject thingOnGun;
+
+    collectible thingIGrabbed;
+
+    Vector3 previousPointerPos;
 
     // Update is called once per frame
     void Update()
     {
-        int layerMask = 1 << 10;
+    /*    int layerMask = 1 << 10;
         layerMask = ~layerMask;
 
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward), out outHit, 100.0f, layerMask))
@@ -50,14 +67,157 @@ public class TreasureHunter : MonoBehaviour
                     }                  
             }      
             scoreSummary.text = "Your Score: " + score;         
-            itemSummary.text = "Number of Items: " + numOfItems;          
+            itemSummary.text = "Number of Items: " + numOfItems;    
+            */
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)){ 
+
+            //### "Force Grab Distance";
+            forceGrab(true);
+          
+        } else if (OVRInput.GetDown(OVRInput.RawButton.A)){
+            //### "Grip";
+            Collider[] overlappingThings=Physics.OverlapSphere(rightPointerObject.transform.position,0.01f,collectiblesMask);
+            if (overlappingThings.Length>0){
+                attachGameObjectToAChildGameObject(overlappingThings[0].gameObject,rightPointerObject,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,true);
+                //I'm not bothering to check for nullity because layer mask should ensure I only collect collectibles.
+                thingIGrabbed=overlappingThings[0].gameObject.GetComponent<collectible>();
+                thingIGrabbed.gameObject.transform.localScale = new Vector3(0.30f, 0.30f, 0.30f);
+            }
+        
+        }else if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger) || OVRInput.GetUp(OVRInput.RawButton.B) ||OVRInput.GetUp(OVRInput.RawButton.RHandTrigger) || OVRInput.GetUp(OVRInput.RawButton.A)) {
+            //"R Index Trigger Pressed Drop";
+            
+            letGo();
+
+        }  else if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger)){
+            // ### "Force Grab Snap";
+            forceGrab(false);
+
+        //##########################################################
+        } else if (OVRInput.GetDown(OVRInput.RawButton.B)){
+            // ### "Magnetic Grip";
+            Collider[] overlappingThings=Physics.OverlapSphere(rightPointerObject.transform.position,1,collectiblesMask);
+            if (overlappingThings.Length>0){
+                collectible nearestCollectible=getClosestHitObject(overlappingThings);
+                attachGameObjectToAChildGameObject(nearestCollectible.gameObject,rightPointerObject,AttachmentRule.SnapToTarget,AttachmentRule.SnapToTarget,AttachmentRule.KeepWorld,true);
+                thingIGrabbed=nearestCollectible.gameObject.GetComponent<collectible>(); //confused?
+            }
+        }
+        previousPointerPos=rightPointerObject.gameObject.transform.position;
+    } //##################################################################
+
+    collectible getClosestHitObject(Collider[] hits){ 
+        float closestDistance=10000.0f;
+        collectible closestObjectSoFar=null;
+        foreach (Collider hit in hits){  // outHit??
+            collectible c=hit.gameObject.GetComponent<collectible>();
+            if (c){
+                float distanceBetweenHandAndObject=(c.gameObject.transform.position-rightPointerObject.gameObject.transform.position).magnitude;
+                if (distanceBetweenHandAndObject<closestDistance){
+                    closestDistance=distanceBetweenHandAndObject;
+                    closestObjectSoFar=c;
+                }
+            }
+        }
+        return closestObjectSoFar;
+    }
+
+    void forceGrab(bool pressedA){
+    RaycastHit outHit;
+    
+    //notice I'm using the layer mask again
+        if (Physics.Raycast(rightPointerObject.transform.position, rightPointerObject.transform.forward, out outHit, 100.0f,collectiblesMask))
+            {
+                AttachmentRule howToAttach=pressedA?AttachmentRule.KeepWorld:AttachmentRule.SnapToTarget;
+                attachGameObjectToAChildGameObject(outHit.collider.gameObject,rightPointerObject.gameObject,howToAttach,howToAttach,AttachmentRule.KeepWorld,true);
+                thingIGrabbed=outHit.collider.gameObject.GetComponent<collectible>();
+                thingIGrabbed.gameObject.transform.localScale = new Vector3(0.30f, 0.30f, 0.30f);
+            } 
+         
+    }
+
+ // ############################################################  NEED TO CHANGE     
+    void letGo(){
+        if (thingIGrabbed){
+            Collider[] overlappingThingsWithLeftHand=Physics.OverlapSphere(leftPointerObject.transform.position,0.01f,collectiblesMask);
+            if (overlappingThingsWithLeftHand.Length>0){
+                if (thingOnGun){
+                    detachGameObject(thingOnGun,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld);
+                    simulatePhysics(thingOnGun,Vector3.zero,true);
+                }
+                attachGameObjectToAChildGameObject(overlappingThingsWithLeftHand[0].gameObject,leftPointerObject,AttachmentRule.SnapToTarget,AttachmentRule.SnapToTarget,AttachmentRule.KeepWorld,true);
+                thingOnGun=overlappingThingsWithLeftHand[0].gameObject;
+                thingIGrabbed=null;
+            }else{
+                detachGameObject(thingIGrabbed.gameObject,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld,AttachmentRule.KeepWorld);
+                simulatePhysics(thingIGrabbed.gameObject,(rightPointerObject.gameObject.transform.position-previousPointerPos)/Time.deltaTime,true);
+                thingIGrabbed=null;
+            }
+        }
+    }
+    //#################################################################################
+
+
+    //since Unity doesn't have sceneComponents like UE4, we can only attach GOs to other GOs which are children of another GO
+    //e.g. attach collectible to controller GO, which is a child of VRRoot GO
+    //imagine if scenecomponents in UE4 were all split into distinct GOs in Unity
+    public void attachGameObjectToAChildGameObject(GameObject GOToAttach, GameObject newParent, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule, bool weld){
+        GOToAttach.transform.parent=newParent.transform;
+        handleAttachmentRules(GOToAttach,locationRule,rotationRule,scaleRule);
+        if (weld){
+            simulatePhysics(GOToAttach,Vector3.zero,false);
+        }
+    }
+
+    public static void detachGameObject(GameObject GOToDetach, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule){
+        //making the parent null sets its parent to the world origin (meaning relative & global transforms become the same)
+        GOToDetach.transform.parent=null;
+        handleAttachmentRules(GOToDetach,locationRule,rotationRule,scaleRule);
+    }
+
+    public static void handleAttachmentRules(GameObject GOToHandle, AttachmentRule locationRule, AttachmentRule rotationRule, AttachmentRule scaleRule){
+        GOToHandle.transform.localPosition=
+        (locationRule==AttachmentRule.KeepRelative)?GOToHandle.transform.position:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (locationRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localPosition:
+        new Vector3(0,0,0);
+
+        //localRotation in Unity is actually a Quaternion, so we need to specifically ask for Euler angles
+        GOToHandle.transform.localEulerAngles=
+        (rotationRule==AttachmentRule.KeepRelative)?GOToHandle.transform.eulerAngles:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (rotationRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localEulerAngles:
+        new Vector3(0,0,0);
+
+        GOToHandle.transform.localScale=
+        (scaleRule==AttachmentRule.KeepRelative)?GOToHandle.transform.lossyScale:
+        //technically don't need to change anything but I wanted to compress into ternary
+        (scaleRule==AttachmentRule.KeepWorld)?GOToHandle.transform.localScale:
+        new Vector3(1,1,1);
+    }
+ 
+    public void simulatePhysics(GameObject target,Vector3 oldParentVelocity,bool simulate){
+        Rigidbody rb=target.GetComponent<Rigidbody>();
+        if (rb){
+            if (!simulate){
+                Destroy(rb);
+            } 
+        } else{
+            if (simulate){
+                //there's actually a problem here relative to the UE4 version since Unity doesn't have this simple "simulate physics" option
+                //The object will NOT preserve momentum when you throw it like in UE4.
+                //need to set its velocity itself.... even if you switch the kinematic/gravity settings around instead of deleting/adding rb
+                Rigidbody newRB=target.AddComponent<Rigidbody>();
+                newRB.velocity=oldParentVelocity;
+            }
+        }      
     }
     //WowWow One
-    public static string GetGameObjectPath(collectible obj)
-    {
-        string path = obj.name;
-        return path;
-    }
+    //public static string GetGameObjectPath(collectible obj)
+    //{
+    //    string path = obj.name;
+    //    return path;
+    //}
 
     
 }
